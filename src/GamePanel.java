c6872bacd6eb48af67ded5001fcb7f08f3768f84
YASHWINLM <league@WTS10.attlocal.net>
2017-05-19 18:31:16 -0700

    mhm

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -39,9 +40,13 @@ void drawEndState(Graphics g){
 }
 
 void drawMenuState(Graphics g){
-	
+	rect(0, 0, 500, 900);
+	fill(250, 250, 250);
 
 }
+
+
+
 
 void updateMenuState(){
 	
