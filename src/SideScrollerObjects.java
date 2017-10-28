7e2c71db30b6be8569e2a32fb81f78b0435146fe
League <League@administorsimac.attlocal.net>
2017-10-28 13:48:52 -0700

    Ok

--- src/SideScrollerObjects.java
+++ src/SideScrollerObjects.java
@@ -1,4 +1,24 @@
+import java.awt.Graphics;
 
 public class SideScrollerObjects {
 
+int x;
+int y;
+int w; 
+int h;
+	
+	
+	SideScrollerObjects(int x, int y, int w, int h){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		
+	}
+	
+	SideScrollerObjects sso = new SideScrollerObjects(x, y, w, h);
+	
+	public void paintComponent(Graphics g){
+		g.fillRect(x, y, w, h);
+	}
 }
--- src/SideScrollerPanel.java
+++ src/SideScrollerPanel.java
@@ -1,27 +1,54 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 
-public class SideScrollerPanel {
+public class SideScrollerPanel extends JPanel implements ActionListener{
 	
 	JFrame frame = new JFrame();
 	final int HEIGHT = 800;
 	final int WIDTH = 1100;
+	Timer time = new Timer(60/1000,this);
 	
-	SideScrollerPanel(){
-	SideScrollerPlayer sideplay = new SideScrollerPlayer();
-	setup();
-		
+	void run(){
+	frame.setSize(WIDTH, HEIGHT);
+	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	time.start();
+	frame.add(this);
+	frame.setVisible(true);
 	}
 	
-	void setup(){
-	frame.setVisible(true);
-	frame.setSize(WIDTH, HEIGHT);
+	
+	ArrayList<SideScrollerObjects> platforms = new ArrayList<SideScrollerObjects>();
+	
+	public void obj() {
 		
+	platforms.add(100, 200, 400, 400);
+	
+	
+	}
+	@Override
+	public void paintComponent(Graphics g){
+		
+		//g.fillRect(100, 200, 400, 400);
 	}
 	
 	
 	public static void main(String [] args){
 	SideScrollerPanel sidepan = new SideScrollerPanel();
-	sidepan.setup();
+	sidepan.run();
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		repaint();
+		
 	}
 }
